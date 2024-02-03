51ad9ca99e55efe796a7d0e583dc47a52c529a03
Freakoverse <136988523+Freakoverse@users.noreply.github.com>
2024-02-03 19:38:26 +0200

    Add files via upload

---
+++ assets/js/openPopUp.js
@@ -0,0 +1,7 @@
+document.getElementById('OpenPopUpMain').addEventListener('click', function() {
+        document.getElementById('PopUpMain').style.display = 'flex';
+    });
+
+    document.getElementById('ClosePopUpMain').addEventListener('click', function() {
+        document.getElementById('PopUpMain').style.display = 'none';
+    });
