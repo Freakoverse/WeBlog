9ced1d374e462ad4d386bb29b77c92e5097fa76b
Freakoverse <136988523+Freakoverse@users.noreply.github.com>
2024-02-05 21:27:23 +0200

    Add files via upload

--- assets/js/openPopUp.js
+++ assets/js/openPopUp.js
@@ -1,7 +1,29 @@
-document.getElementById('OpenPopUpMain').addEventListener('click', function() {
-        document.getElementById('PopUpMain').style.display = 'flex';
+//pop-up for relay
+document.getElementById('OpenPopUpMainRelay').addEventListener('click', function() {
+        document.getElementById('PopUpMainRelay').style.display = 'flex';
+        document.body.style.overflow = 'hidden';
     });
 
-    document.getElementById('ClosePopUpMain').addEventListener('click', function() {
-        document.getElementById('PopUpMain').style.display = 'none';
+    document.getElementById('ClosePopUpMainRelay').addEventListener('click', function() {
+        document.getElementById('PopUpMainRelay').style.display = 'none';
+        document.body.style.overflow = 'auto';
+    });
+
+
+
+//pop-up for comment replies
+    // Get all elements with the specified class
+    var openButtons = document.querySelectorAll('.HBLCL_CAEReplies');
+
+    // Add click event listeners to all elements with the specified class
+    openButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            document.getElementById('PopUpMainCommentReplies').style.display = 'flex';
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    document.getElementById('ClosePopUpMainCommentReplies').addEventListener('click', function() {
+        document.getElementById('PopUpMainCommentReplies').style.display = 'none';
+        document.body.style.overflow = 'auto';
     });
